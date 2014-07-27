babd4cb909642fbf7e41be3b3fe91448fbe9177a
JohnnyPP <johnnnyppp@gmail.com>
2014-07-27 17:54:21 +0200

    Added some documentation

--- temp/dektakWithoutMain.py
+++ temp/dektakWithoutMain.py
@@ -5,7 +5,6 @@ import sys
 import math
 from numpy import NaN, Inf, arange, isscalar, asarray, array
 
-
 def FindHeaderLength(file_path, file_name):
     """
     Finds the position of the 'Scan Data' and adds additional 4 lines
@@ -99,15 +98,64 @@ def peakdet(v, delta, x=None):
 
 
 def coefA(x0, y0, x1, y1):
+    """
+    Finds the coefficient A of the line going thorugh the two points P0 (x0,y0),
+    P1 (x1,y1). Run dektakTest.py for a simple example
+    :param x0: 
+    :param y0:
+    :param x1:
+    :param y1:
+    :return: coefA = -(y1 - y0) / (x1 - x0)
+    
+    http://en.wikipedia.org/wiki/Linear_equation
+    General (or standard) form
+
+    In the general (or standard[1]) form the linear equation is written as:
+
+    coefAx + coefBy = coefC, \,
+
+    where A and B are not both equal to zero. The equation is usually written 
+    so that A >= 0, by convention. The graph of the equation is a straight line,
+    and every straight line can be represented by an equation in the above 
+    form. If A is nonzero, then the x-intercept, that is, the x-coordinate of 
+    the point where the graph crosses the x-axis (where, y is zero), is C/A. 
+    If B is nonzero, then the y-intercept, that is the y-coordinate of the 
+    point where the graph crosses the y-axis (where x is zero), is C/B, and 
+    the slope of the line is -A/B. The general form is sometimes written as:
+    ax + by + c = 0
+
+    where a and b are not both equal to zero. The two versions can be 
+    converted from one to the other by moving the constant term to the other 
+    side of the equal sign.
+
+    """
     return -(y1 - y0) / (x1 - x0)
 
 
 def coefC(x0, y0, x1, y1):
+    """
+    Finds the coefficient C of the line going thorugh the two points P0 (x0,y0),
+    P1 (x1,y1). Run dektakTest.py for a simple example
+    :param x0: 
+    :param y0:
+    :param x1:
+    :param y1:
+    :return: coefC = -(x1 * y0 - x0 * y1) / (x1 - x0)
+    """
     return (x1 * y0 - x0 * y1) / (x1 - x0)
 
 
 def FindThresholdLine(x, y, threshold):
-
+    """
+    Finds the intersection points for a given x, y data that lie on a threshold
+    line. This is a kind of interlopation of the x data as the threshold is
+    given for the y axis.
+    :param x:
+    :param y:
+    :param threshold:
+    :return: incIntersectionPoints, decIntersectionPoints
+    """
+    
     increasingPoints = []
     incLineEquationCoefficients = []
     incIntersectionPoints = []
@@ -115,7 +163,6 @@ def FindThresholdLine(x, y, threshold):
     decreasingPoints = []
     decIntersectionPoints = []
 
-    thresholdLineArray = np.array([0, 1, threshold])
     for i in xrange(0, len(y)):
         if i < (len(y) - 1):
             if y[i] < threshold and y[i + 1] > threshold:  # incerasing
@@ -127,10 +174,9 @@ def FindThresholdLine(x, y, threshold):
                 coefAinc = coefA(x0, y0, x1, y1)
                 coefCinc = coefC(x0, y0, x1, y1)
                 incPointsCrossingThreshold = np.array([x0, y0, x1, y1])
-                lineEquationCooficientsCrossongThr = np.array([coefAinc, 1, coefCinc])
+                lineEquationCoefficientsCrossongThr = np.array([coefAinc, 1, coefCinc])
                 increasingPoints.append(incPointsCrossingThreshold)
-                incLineEquationCoefficients.append(lineEquationCooficientsCrossongThr)
-
+                incLineEquationCoefficients.append(lineEquationCoefficientsCrossongThr)
             else:
                 if y[i] > threshold and y[i + 1] < threshold:  # decreasing
                     #print "Decreasing line detected"
@@ -141,35 +187,70 @@ def FindThresholdLine(x, y, threshold):
                     coefAdec = coefA(x0, y0, x1, y1)
                     coefCdec = coefC(x0, y0, x1, y1)
                     decPointsCrossingThreshold = np.array([x0, y0, x1, y1])
-                    lineEquationCooficientsCrossongThrDec = np.array([coefAdec, 1, coefCdec])
+                    lineEquationCoefficientsCrossongThrDec = np.array([coefAdec, 1, coefCdec])
                     decreasingPoints.append(decPointsCrossingThreshold)
-                    decLineEquationCoefficients.append(lineEquationCooficientsCrossongThrDec)
+                    decLineEquationCoefficients.append(lineEquationCoefficientsCrossongThrDec)
                     #else:
                     #print "Neither dereasing nor incereasing line"
-
+                    
         else:
             break
 
-    A2 = thresholdLineArray[0]
+    thresholdLineArray = np.array([0, 1, threshold])    # coefA=0, coefB=1
+                                                        # coefC=threshold
+    A2 = thresholdLineArray[0]  # threshold line coeficients
     B2 = thresholdLineArray[1]
     C2 = thresholdLineArray[2]
 
-    for i in xrange(0, incLineEquationCoefficients.__len__()):
-        A1 = incLineEquationCoefficients[i][0]
-        B1 = incLineEquationCoefficients[i][1]
-        C1 = incLineEquationCoefficients[i][2]
+    """
+    The intersection points may be calculated when both line equations are
+    known (line coefficients). This is done separately for increasing line:
+    incA1, incB1, incC1 coeficients 
+    and decreasing line coefficients:
+    decA1, decB1, decC1 
+    Threshold line coefficients:
+    A2, B2, C2
+    
+    The intersection points are calculated by means of determinant and
+    linear system of equations:
+    http://en.wikipedia.org/wiki/Determinant
+    http://en.wikipedia.org/wiki/System_of_linear_equations
+    http://www.cliffsnotes.com/math/algebra/algebra-ii/linear-sentences-in-two-
+    variables/linear-equations-solutions-using-determinants-with-two-variables
+    
+    W=|A1B1
+       A2B2|=A1*B2-A2*B1
+    Wx=|-C1B1
+        -C2B2|=(-C1)*B2-(-C2)*B1   #determinant [[5, 0], [6, 1]]
+    Wy=|A1-C1
+        A2-C2|=A1*(-C2)-A2*(-C1)   #determinant [[4, 5], [1, 6]]
 
-        detW = float(A1 * B2 - A2 * B1)
-        detWx = float((C1) * B2 - (C2) * B1)
-        detWy = float(A1 * (C2) - A2 * (C1))
+    Ax+By+C=0
+    A=(y_1-y_0)/(x_1-x_0)
+    B=1 always
+    C=(x_1*y_0-x_0*y_1)/(x_1-x_0)
+    
+    Geometric interpretation
+
+    For a system involving two variables (x and y), each linear equation 
+    determines a line on the xy-plane. Because a solution to a linear system 
+    must satisfy all of the equations, the solution set is the intersection 
+    of these lines, and is hence either a line, a single point, 
+    or the empty set. We are satisfied with single point solution.
+    """
+    
+    for i in xrange(0, incLineEquationCoefficients.__len__()):
+        incA1 = incLineEquationCoefficients[i][0]
+        incB1 = incLineEquationCoefficients[i][1]
+        incC1 = incLineEquationCoefficients[i][2]
+
+        detW = float(incA1 * B2 - A2 * incB1)
+        detWx = float((incC1) * B2 - (C2) * incB1)
+        detWy = float(incA1 * (C2) - A2 * (incC1))
         pointX = float(detWx / detW)
         pointY = float(detWy / detW)
         incIntersectionPoints.append(np.array([pointX, pointY]))
-        # plt.plot(xDiff[i+start],incIntersectionPoints[i][1],'go')
-
-
-        #plt.plot(incIntersectionPoints[i][0],incIntersectionPoints[i][1],'go')
-
+        
     for i in xrange(0, decLineEquationCoefficients.__len__()):
         decA1 = decLineEquationCoefficients[i][0]
         decB1 = decLineEquationCoefficients[i][1]
@@ -181,12 +262,6 @@ def FindThresholdLine(x, y, threshold):
         decpointX = float(decdetWx / decdetW)
         decpointY = float(decdetWy / decdetW)
         decIntersectionPoints.append(np.array([decpointX, decpointY]))
-        # plt.plot(xDiff[i+start],decIntersectionPoints[i][1],'ro')
-        #plt.plot(decIntersectionPoints[i][0]+x[start]/2,decIntersectionPoints[i][1],'ro')
-
-        #plt.plot(decIntersectionPoints[i][0],decIntersectionPoints[i][1],'ro')
-
-
 
     return incIntersectionPoints, decIntersectionPoints
 
@@ -228,8 +303,8 @@ def load_data(file_name, file_path):
 
 def surface_tilt_levelling(x, y):
     """
-    Performs polynomial fit to the tilted profile and returns the difference between raw data and the fitted
-    curve what levels (straightens) the profile
+    Performs polynomial fit to the tilted profile and returns the difference 
+    between raw data and the fitted curve what levels (straightens) the profile
     :param x: raw data
     :param y: raw data
     :return: yLevelled levelled profile
@@ -542,27 +617,34 @@ def calculate_start_slice(x_axis, start_slice):
 
 #######################################################################################################################
 #######################################################################################################################
+# parameters to set
 
 file_name = 't10_1_1_normal.csv'
 file_path = '/home/kolan/mycode/python/dektak/data/'
+moving_average_size = 277
+first_harmonics_to_keep = 1500
+find_peak_threshold = 0.065            # reliable results between 0.05 and 0.09
 
+#
 x, y = load_data(file_name, file_path)
 
 yLevelled = surface_tilt_levelling(x, y)
 
-yLevelMovingAverage = moving_average(x, yLevelled, 277)
+yLevelMovingAverage = moving_average(x, yLevelled, moving_average_size)
 
 averageStructuresHeight, maxHarmonic = amplitude_and_phase_FFT(yLevelMovingAverage)
 
 xDiff, yDiff = first_order_difference(x, yLevelMovingAverage)
 
-yCalculatedIFFTFiltered = FFT_filtering(xDiff, yDiff, 1500)
+yCalculatedIFFTFiltered = FFT_filtering(xDiff, yDiff, first_harmonics_to_keep)
 
-maxtab, mintab = find_minima_and_maxima(xDiff, yCalculatedIFFTFiltered, 0.065, maxHarmonic)
+maxtab, mintab = find_minima_and_maxima(xDiff, yCalculatedIFFTFiltered, find_peak_threshold, maxHarmonic)
 
 ##############################################################################
-##Slicing
-increaseSliceLength = 200 # this is in index
+## Slicing
+
+increaseSliceLength = 200           # this is in index
+
 
 widthTop = []
 widthBottom = []
@@ -589,14 +671,13 @@ fig.subplots_adjust(hspace=.5, wspace=.2)
 axs = axs.ravel()
 plt.suptitle('Sliced structures: x Lateral [um], y Raw Micrometer [um]')
 
-thresholdLengthStep = 0.002
-
-thresholdStep = 0.001
 signalIFFT = np.column_stack((xDiff, yCalculatedIFFTFiltered))
-xIFFT = signalIFFT[:, 0]
+xIFFT = signalIFFT[:, 0]    #keeps the xDiff, yCalculatedIFFTFiltered results in one np.array
 yIFFT = signalIFFT[:, 1]
 
-'''
+thresholdLengthStep = 0.002
+thresholdStep = 0.001
+
 for thresholdLength in (np.arange(0.0, 0.04, thresholdLengthStep)):
     for sliceNumber in range(maxHarmonic):
 
@@ -659,9 +740,10 @@ fig, axs = plt.subplots(5, 4, figsize=(15, 6), facecolor='w', edgecolor='k')
 fig.subplots_adjust(hspace=.5, wspace=.2)
 axs = axs.ravel()
 plt.suptitle('Sliced structures last: x Lateral [um], y Raw Micrometer [um]')
-'''
 
-minThresholdLength = 0
+
+##############################################################################
+# The last run with the minThresholdLength found by the scanning above
 
 for sliceNumber in range(maxHarmonic):
 
@@ -710,19 +792,21 @@ for sliceNumber in range(maxHarmonic):
                              aincNegativeLast, yLevelled, axs)
                              
 for i in range(maxHarmonic-1):
+    # appends the spaceTop and spaceBottom
     spaceTop.append(aincPositveLastList[i+1]-aincNegativeLastList[i])
     spaceBottom.append(adecPositveLastList[i+1]-adecNegativeLastList[i])
-
-for i in range(maxHarmonic):
-    xlineListTop.append(adecNegativeLastList[i])
-    xlineListTop.append(adecPositveLastList[i])
-    xlineListBottom.append(aincNegativeLastList[i])
-    xlineListBottom.append(aincPositveLastList[i])
     
-    ylineListTop.append(yAdecNegativeLastList[i])
+for i in range(maxHarmonic):
+    # appends the lines for the Full raw data with points plot
+    xlineListTop.append(adecPositveLastList[i])
+    xlineListTop.append(adecNegativeLastList[i])
+    xlineListBottom.append(aincPositveLastList[i])
+    xlineListBottom.append(aincNegativeLastList[i])
+    
     ylineListTop.append(yAdecPositveLastList[i])
-    ylineListBottom.append(yAincNegativeLastList[i])
+    ylineListTop.append(yAdecNegativeLastList[i])
     ylineListBottom.append(yAincPositveLastList[i])
+    ylineListBottom.append(yAincNegativeLastList[i])
     
 
 npWidthTop = np.array(widthTop)
